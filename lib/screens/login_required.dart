bea8eec63b2a72f984ab0ddb342a991654f63e1e
Magnolia Fayza <magnolia.fayza@yahoo.com>
2021-12-30 21:44:18 +0700

    insyaAllah done

---
+++ lib/screens/login_required.dart
@@ -0,0 +1,108 @@
+import 'package:b07uas/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class CustomDialog extends StatelessWidget {
+  final String title, description, buttonText;
+  // final Image image;
+
+  CustomDialog({
+    this.title = "Login",
+    this.description =
+        "Kamu harus login terlebih dahulu untuk mengakses fitur ini",
+    this.buttonText = "Ok",
+    // this.image = ""
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      child: dialogContent(context),
+    );
+  }
+
+  dialogContent(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
+            top: 130.0,
+            bottom: 16.0,
+            left: 16.0,
+            right: 16.0,
+          ),
+          margin: EdgeInsets.only(top: 66.0),
+          decoration: new BoxDecoration(
+            color: Colors.grey[200], //Colors.black.withOpacity(0.3),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(16.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                offset: const Offset(0.0, 10.0),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // To make the card compact
+            children: <Widget>[
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w700,
+                  // color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                description,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  // color: Colors.white70,
+                ),
+              ),
+              SizedBox(height: 24.0),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    // Navigator.of(context).pop();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()),
+                    );
+                  },
+                  child: Text(
+                    buttonText,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          left: 16.0,
+          right: 16.0,
+          child: CircleAvatar(
+            backgroundColor: Colors.grey,
+            radius: 80,
+            backgroundImage: NetworkImage(
+              '<https://upload.wikimedia.org/wikipedia/commons/1/1d/Rotating_Konarka_chaka.gif>',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/regulasi/regulasi_screen.dart
+++ lib/screens/regulasi/regulasi_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:b07uas/widgets/main_drawer.dart';
 import 'package:b07uas/widgets/regulasi/country_form.dart';
+import 'package:b07uas/screens/user.dart' as user;
+import 'package:b07uas/screens/login_required.dart';
 
 class RegulasiScreen extends StatelessWidget {
   static const routeName = '/regulasi';
@@ -8,17 +10,20 @@ class RegulasiScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('Regulasi'),
-      ),
-      drawer: MainDrawer(),
-      body: Center(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(32),
-          child: const CountryForm(),
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('Regulasi'),
         ),
-      )
-    );
+        drawer: MainDrawer(),
+        body: user.user[0]['status'] == 'logged off'
+            ? Center(
+                child: CustomDialog()
+              )
+            : Center(
+                child: SingleChildScrollView(
+                  padding: EdgeInsets.all(32),
+                  child: const CountryForm(),
+                ),
+              ));
   }
 }
