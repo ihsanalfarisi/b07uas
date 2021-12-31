18e8076ad420e9ad695f3aa00091c562dfb5e52a
ameliarani <amelia.maharani02@ui.ac.id>
2021-12-31 22:17:17 +0700

    add hotel url launcher

--- lib/screens/hotel/hotel.dart
+++ lib/screens/hotel/hotel.dart
@@ -1,19 +1,21 @@
 // ignore_for_file: sort_constructors_first
 
 class Hotel {
+  final String? country;
   final String? namaHotel;
   final String? foto;
   final String? harga;
-  final String? country;
+  final String? detail;
 
-  Hotel._({this.namaHotel, this.foto, this.harga, this.country});
+  Hotel._({this.country, this.namaHotel, this.foto, this.harga, this.detail});
 
   factory Hotel.fromJson(Map<String, dynamic> json) {
     return Hotel._(
+      country: json['fields']['country'],
       namaHotel: json['fields']['nama_hotel'],
       foto: json['fields']['foto'],
       harga: json['fields']['harga'],
-      country: json['fields']['country'],
+      detail: json['fields']['detail'],
     );
   }
 }
